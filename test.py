33d7675a1e170c696ad27491dc4dc85fb4122179
eva-burns <evasophia@gmail.com>
2023-05-09 15:39:02 -0500

    added filter by trip # and avg veloccity

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import os
 
 st.set_page_config(layout="wide")
 st.title('Sample AV Dashboard')
@@ -9,9 +10,35 @@ st.title('Sample AV Dashboard')
 DATE_COLUMN = 'Time'
 @st.cache_data
 def load_data():
-    df = pd.read_csv("../AV Sample Data/gps.csv")[['Time', 'latitude', 'longitude', 'altitude']]
+    f = []
+    w = os.walk("./Data")
+    for (dirpath, dirnames, filenames) in w:
+        f.extend(dirnames)
 
-    # df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN])
+    gps_files = [f'./Data/{dir}/gps.csv' for dir in f]
+    velocity_files = [f'./Data/{dir}/velocity.csv' for dir in f]
+
+    df = pd.DataFrame()
+    i = 1
+    for file in gps_files:
+        temp_df = pd.read_csv(file)[['Time', 'latitude', 'longitude', 'altitude']]
+        temp_df['trip number'] = i
+        df = pd.concat([df, temp_df], axis=0)
+        i += 1
+    df['Time'] = df['Time'].round(3)
+
+    vel_df = pd.DataFrame()
+    i = 1
+    for file in velocity_files:
+        temp_df = pd.read_csv(file)[['Time', 'data']]
+        vel_df = pd.concat([vel_df, temp_df], axis=0)
+        i += 1
+    vel_df['Time'] = vel_df['Time'].round(3)
+
+    df = pd.merge(df, vel_df, on='Time')
+    df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN],unit='s')
+    # vel_df[DATE_COLUMN] = pd.to_datetime(vel_df[DATE_COLUMN],unit='s')
+    df['Date of Trip'] = df[DATE_COLUMN].dt.normalize()
     return df
 
 data = load_data()
@@ -21,19 +48,17 @@ data = load_data()
 
 col1, col2 = st.columns([3, 1])
 
-with st.sidebar:
-    vals = sorted(set(data['Time']))
 
-    start_color, end_color = st.select_slider(
-        'Select a range of color wavelength',
-        key='slider',
-        options=vals,
-        value=(vals[0], vals[len(vals)-1]))
-    st.write(start_color)
-    
-    filtered_data = data[data['Time'] >= start_color]
-    filtered_data = filtered_data[filtered_data['Time'] <= end_color]
-    # st.empty()
+with st.sidebar:
+    date_choices = list(set(data['trip number']))
+    # date_choices = pd.DataFrame(date_choices)
+    # date_choices['Date'] = [df['Date'][0] for df in ]
+    selectbox_state = st.multiselect("Choose a date", date_choices, date_choices)
+    if len(selectbox_state) == 0:
+        filtered_data = data.copy()
+        st.write("Please enter trip number")
+    else:
+        filtered_data = data[data['trip number'].isin(selectbox_state)]
 
 with col1:
     midpoint = ((np.max(filtered_data['latitude']) + np.min(filtered_data['latitude']))/2, (np.max(filtered_data['longitude']) + np.min(filtered_data['longitude']))/2)
@@ -43,7 +68,7 @@ with col1:
         initial_view_state=pdk.ViewState(
             latitude=midpoint[0],
             longitude=midpoint[1],
-            zoom=12,
+            zoom=15,
             pitch=0,
         ),
         layers=[
@@ -58,6 +83,6 @@ with col1:
     ))
 
 with col2:
-    st.metric(label="Average Velocity", value=123)
+    st.metric(label="Average Velocity", value=round(np.mean(filtered_data['data']), 4))
     
-    st.write(filtered_data)
+    st.write(data)
