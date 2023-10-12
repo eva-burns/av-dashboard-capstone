70522f62e266efbf572483326880d9519cabd2fb
eva-burns <evasophia@gmail.com>
2023-10-12 10:43:25 -0500

    updated dashboard, data removed

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import numpy as np
 import pydeck as pdk
 import os
 from datetime import datetime
+import math
 
 st.set_page_config(layout="wide")
 st.title('Sample AV Dashboard')
@@ -15,10 +16,10 @@ def load_data():
     w = os.walk("./Data")
     for (dirpath, dirnames, filenames) in w:
         f.extend(dirnames)
-    dirnames = sorted(dirnames)
-    gps_files = [f'./Data/{dir}/gps.csv' for dir in f]
-    velocity_files = [f'./Data/{dir}/velocity.csv' for dir in f]
-    mode_files = [f'./Data/{dir}/vehiclemode.csv' for dir in f]
+    directories = sorted(f)
+    gps_files = [f'./Data/{dir}/gps.csv' for dir in directories]
+    velocity_files = [f'./Data/{dir}/velocity.csv' for dir in directories]
+    mode_files = [f'./Data/{dir}/vehiclemode.csv' for dir in directories]
 
     df = pd.DataFrame()
     i = 1
@@ -45,13 +46,27 @@ def load_data():
         temp_df = pd.read_csv(file)[['Time', 'data']]
         vel_df = pd.concat([vel_df, temp_df], axis=0)
         i += 1
+    
+    vel_df['Timestamp'] = vel_df['Time'].copy()
     vel_df['Time'] = vel_df['Time'].round(3)
+
+    vel_df = vel_df.groupby('Time').agg({'latitude':'mean', 'longitude':'mean', 'altitude':'mean', 'trip number': 'min', 'mode':'min', 'velocity':'mean', 'Timestamp':'mean'})
+
     vel_df.rename(columns={'data': 'velocity'}, inplace=True)
     df = pd.merge(df, vel_df, on='Time')
-
+    
     df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN],unit='s')
 
     df = df.sort_values(by=['Time'], ignore_index=True)
+
+    df.reset_index(inplace=True)
+    df['Date'] = df['Time'].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+    unique_times = df['Date'].unique()
+    unique_times = dict(zip(unique_times,list(range(1, len(unique_times)+1))))
+
+    # df['trip number'] = df['Time'].apply(lambda x: unique_times[x.strftime('%Y-%m-%d')])
+    
     
     return df
 
@@ -59,21 +74,41 @@ data = load_data()
 
 @st.cache_data
 def get_trip_choices():
-    return sorted(list(set(data['trip number'])))
+    return sorted(list(data['trip number'].unique()))
 
 date_choices = get_trip_choices()
 
 def trip_formatter(trip_num):
-    date = list(data.loc[data['trip number'] == trip_num,'Time'])[0].floor('s')
+    date = data[data['trip number'] == 1]['Date'].head(1)[0]
     new_str = f'Trip {trip_num} ({date})'
     return new_str
 
 col1, col2 = st.columns([3, 1])
 
+# layer = pdk.Layer(
+#     "TripsLayer",
+#     data,
+#     get_path="[longitude, latitude]",
+#     get_timestamps="Timestamp",
+#     get_color=[253, 128, 93],
+#     width_min_pixels=5,
+#     rounded=True,
+#     trail_length=100000000000,
+#     current_time=int(np.max(list(data["Timestamp"]))),
+# )
+# midpoint = ((np.max(data['latitude']) + np.min(data['latitude']))/2, (np.max(data['longitude']) + np.min(data['longitude']))/2)
+
+# view_state = pdk.ViewState(latitude=midpoint[0],
+#             longitude=midpoint[1],
+#             zoom=15,
+#             pitch=0)
+
+# # Render
+# r = pdk.Deck(layers=[layer], initial_view_state=view_state, map_style=None)
+# st.pydeck_chart(r)
 
 with st.sidebar:
-    
-    selectbox_state = st.multiselect("Choose a date", date_choices,  date_choices, format_func=trip_formatter)
+    selectbox_state = st.multiselect("Choose a date", date_choices,  default=date_choices[10], format_func=trip_formatter,)
     if len(selectbox_state) == 0:
         filtered_data = data.copy()
         st.write("Please enter trip number")
@@ -105,5 +140,20 @@ with col1:
 with col2:
     st.metric(label="Average Velocity", value=round(np.mean(filtered_data['velocity']), 4))
 
-    st.write(data)
+    group_trip = filtered_data.groupby('trip number').agg(min_time = ('Time', 'min'), max_time = ('Time', 'max'))
+    tot_seconds = (group_trip['max_time'] - group_trip['min_time']).dt.total_seconds()
+    mean_seconds = tot_seconds.mean()
+    hours = int(np.floor(((mean_seconds) / 60 / 60)))
+    mean_seconds -= hours * 60 * 60
+    minutes = int(np.floor(((mean_seconds) / 60)))
+    mean_seconds -= minutes * 60
+    seconds = int(np.floor(((mean_seconds))))
+
+    st.metric('Mean Duration of Trip(s)', value=f'{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}')
+
+    st.write(filtered_data)
+
     
+    
+    # filtered_data['Simplified Time'] = filtered_data['Time'].dt.second
+   
